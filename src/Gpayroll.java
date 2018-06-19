e6a2d80faec3371c14e7ab2f701798e327c9a72d
NareshRoyal <naresh>
2018-06-19 04:59:53 -0700

    i created src folder

---
+++ src/Gpayroll.java
@@ -0,0 +1,3 @@
+This is the file called Gpayroll.java. It is using for payroll process they can easyly write this file.
+may it has to be facilitate by the payroll action.
+
---
+++ src/index.java
@@ -0,0 +1,7 @@
+Hi This is Naresh,
+
+ Just now i stated practicing by Git.
+ This is very confiuging now.because before 1 years only i was practiced.
+ So it is very critical now.
+It's ok i wil learn very queqly.
+
